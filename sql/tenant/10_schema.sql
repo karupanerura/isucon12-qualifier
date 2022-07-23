61b9cbd2719cc0caecd1412b0160c47d44b6a832
karupanerura <karupa@cpan.org>
2022-07-23 17:14:53 +0900

    fix

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE `competition` (
   `created_at` bigint NOT NULL,
   `updated_at` bigint NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `player` (
   `id` bigint NOT NULL,
