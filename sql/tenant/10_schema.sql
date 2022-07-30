ec0ab3c91f5ef41c98c7e1b0d32b5374b1978a9a
karupanerura <karupa@cpan.org>
2022-07-30 10:16:26 +0900

    schema fix

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS player_score;
 
 CREATE TABLE `competition` (
   `id` bigint NOT NULL,
-  `tenant_id` bigint NOT NULL,
   `title` text NOT NULL,
   `finished_at` bigint DEFAULT NULL,
   `created_at` bigint NOT NULL,
@@ -14,7 +13,6 @@ CREATE TABLE `competition` (
 
 CREATE TABLE `player` (
   `id` bigint NOT NULL,
-  `tenant_id` bigint NOT NULL,
   `display_name` text NOT NULL,
   `is_disqualified` tinyint(1) NOT NULL,
   `created_at` bigint NOT NULL,
@@ -23,12 +21,12 @@ CREATE TABLE `player` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `player_score` (
-  `tenant_id` bigint NOT NULL,
   `player_id` bigint NOT NULL,
   `competition_id` bigint NOT NULL,
   `score` bigint NOT NULL,
+  `row_num` bigint NOT NULL,
   `created_at` bigint NOT NULL,
   `updated_at` bigint NOT NULL,
-  PRIMARY KEY (`tenant_id`, `player_id`, `competition_id`),
-  INDEX `ranking_idx` (`tenant_id`, `competition_id`, `score`)
+  PRIMARY KEY (`player_id`, `competition_id`),
+  INDEX `ranking_idx` (`competition_id`, `score`, `row_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
