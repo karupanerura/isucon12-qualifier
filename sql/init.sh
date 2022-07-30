5fac1bed5662ef8fa6bac642b8bf81b4a9c4a490
karupanerura <karupa@cpan.org>
2022-07-30 10:55:00 +0900

    fix

--- sql/init.sh
+++ sql/init.sh
@@ -3,7 +3,6 @@
 set -ex
 cd `dirname $0`
 
-ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
 ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
@@ -12,10 +11,17 @@ ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
+		--host "$ISUCON_ADMIN_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < init.sql
-gunzip -c tenants.sql.gz | mysql -u"$ISUCON_DB_USER" \
+		"$ISUCON_DB_NAME" < init.sql &
+
+gunzip -c tenants1.sql.gz | mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT"
+		--host "$ISUCON_TENANT1_DB_HOST" \
+		--port "$ISUCON_DB_PORT" &
+gunzip -c tenants2.sql.gz | mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_TENANT2_DB_HOST" \
+		--port "$ISUCON_DB_PORT" &
+
+wait
