cecdc3b1b81babe67e3939d1023dfc95f39c1077
karupanerura <karupa@cpan.org>
2022-07-30 10:56:04 +0900

    fix

--- sql/init.sh
+++ sql/init.sh
@@ -9,18 +9,15 @@ ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
 # MySQLを初期化
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
+env MYSQL_PWD="$ISUCON_DB_PASSWORD" mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_ADMIN_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql &
 
-gunzip -c tenants1.sql.gz | mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
+gunzip -c tenants1.sql.gz | env MYSQL_PWD="$ISUCON_DB_PASSWORD" mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_TENANT1_DB_HOST" \
 		--port "$ISUCON_DB_PORT" &
-gunzip -c tenants2.sql.gz | mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
+gunzip -c tenants2.sql.gz | env MYSQL_PWD="$ISUCON_DB_PASSWORD" mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_TENANT2_DB_HOST" \
 		--port "$ISUCON_DB_PORT" &
 
